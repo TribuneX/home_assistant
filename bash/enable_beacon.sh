7d2df86200f115a9529286053891fb921f2df982
TribuneX <sascha@cbs-service.net>
2018-01-04 22:47:27 +0100

    Disable old beacon before cosntructing new beacon

--- bash/enable_beacon.sh
+++ bash/enable_beacon.sh
@@ -4,5 +4,6 @@
 # Major: 00 01 
 # Minor: 00 02
 
+sudo hcitool -i hci0 cmd 0x08 0x000a 00
 sudo hcitool -i hci0 cmd 0x08 0x0008 1e 02 01 1a 1a ff 4c 00 02 15 e2 c5 6d b5 df fb 48 d2 b0 60 d0 f5 a7 10 96 e1 00 01 00 02 c8 00
 sudo hcitool -i hci0 cmd 0x08 0x000a 01 
